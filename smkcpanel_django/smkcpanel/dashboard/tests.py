5b1d33f90c1cd3a9d9d2cb8f885b9f547c3dc700
SamukaSMk <samukasmk@gmail.com>
2013-09-29 18:23:24 -0300

    Single tests - OK

--- smkcpanel_django/smkcpanel/dashboard/tests.py
+++ smkcpanel_django/smkcpanel/dashboard/tests.py
@@ -5,12 +5,66 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
+
+class DashboardTestCase(TestCase):
+
+	def setUp(self):
+		# Every test needs a client.
+		self.client = Client()
+
+	def test_dashboard_home_basic_access(self):
+
+		# Issue a GET request.
+		response = self.client.get('/server/')
+
+		# Check that the rendered content contains 'Aloha, My Friend!'
+		text = 'Aloha, My Friend!'
+		self.assertContains(response, text, count=None, status_code=200)
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+	def test_dashboard_say_my_name_default(self):
+
+		# Issue a GET request.
+		response = self.client.get('/server/say_my_name/PutYouNameHere')
+
+		# Check that the rendered content contains 'Aloha, My Friend!'
+		text = 'Hello, PutYouNameHere !'
+		self.assertContains(response, text, count=None, status_code=200)
+
+
+	def test_dashboard_say_my_name_specific(self):
+
+		# Issue a GET request.
+		response = self.client.get('/server/say_my_name/smk')
+
+		# Check that the rendered content contains 'Aloha, My Friend!'
+		text = 'Hello, smk !'
+		self.assertContains(response, text, count=None, status_code=200)
+
+
+
+	def test_dashboard_say_my_name_Heisenberg_redirect(self):
+
+		# Issue a GET request.
+		response = self.client.get('/server/say_my_name/')
+
+		expected_url = '/server/say_my_name/Heisenberg'
+
+		# Check that the response is 200 OK.
+		self.assertRedirects(response, expected_url, status_code=302, target_status_code=200)
+
+
+	def test_dashboard_say_my_name_Heisenberg_page(self):
+
+		# Issue a GET request.
+		response = self.client.get('/server/say_my_name/Heisenberg')
+
+		# Check that the rendered content contains 'Aloha, My Friend!'
+		text = 'Hello, Heisenberg !'
+		self.assertContains(response, text, count=None, status_code=200)
+
+		# Check that the rendered content contains 'Aloha, My Friend!'
+		text = 'Heisenberg is the smartest bad ever'
+		self.assertContains(response, text, count=None, status_code=200)
--- smkcpanel_django/smkcpanel/tests.py
+++ smkcpanel_django/smkcpanel/tests.py
@@ -1,16 +1,68 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+# """
+# This file demonstrates writing tests using the unittest module. These will pass
+# when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
-"""
+# Replace this with more appropriate tests for your application.
+# """
 
-from django.test import TestCase
+# from django.test import TestCase, Client
+
+# class StaticFilesTestCase(TestCase):
+# 	"""docstring for StaticFilesTestCase"""
+# 	def setUp(self):
+# 		# Every test needs a client.
+# 		self.client = Client()
+
+# 	def test_bootstrap_css(self):
+# 		# Issue a GET request.
+# 		response = self.client.get('/static/bootstrap/extra/bootswatch/cerulean/css/bootstrap.css')
+
+# 		# Check that the response is 200 OK.
+# 		self.assertEqual(response.status_code, 200)
+		
+
+# 	def test_bootstrap_css_min(self):
+# 		# Issue a GET request.
+# 		response = self.client.get('/static/bootstrap/extra/bootswatch/cerulean/css/bootstrap.min.css')
+
+# 		# Check that the response is 200 OK.
+# 		self.assertEqual(response.status_code, 200)
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+# 	def test_bootstrap_js_min(self):
+# 		# Issue a GET request.
+# 		response = self.client.get('/static/bootstrap/dist/js/bootstrap.min.js')
+
+# 		# Check that the response is 200 OK.
+# 		self.assertEqual(response.status_code, 200)
+
+
+# 	def test_jquery_js(self):
+# 		# Issue a GET request.
+# 		response = self.client.get('/static/bootstrap/assets/js/jquery.js')
+
+# 		# Check that the response is 200 OK.
+# 		self.assertEqual(response.status_code, 200)
+
+# 	def test_jquery_min_map(self):
+# 		# Issue a GET request.
+# 		response = self.client.get('/static/bootstrap/assets/js/jquery-1.10.2.min.map')
+
+# 		# Check that the response is 200 OK.
+# 		self.assertEqual(response.status_code, 200)
+
+
+# 	def test_font_awesome_css(self):
+# 		# Issue a GET request.
+# 		response = self.client.get('/static/bootstrap/extra/font-awesome/css/font-awesome.min.css')
+
+# 		# Check that the response is 200 OK.
+# 		self.assertEqual(response.status_code, 200)
+
+
+# 	def test_twitter_widgets_js(self):
+# 		# Issue a GET request.
+# 		response = self.client.get('/static/bootstrap/extra/twitter/widgets.js')
+
+# 		# Check that the response is 200 OK.
+# 		self.assertEqual(response.status_code, 200)
